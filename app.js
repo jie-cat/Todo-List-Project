cb0b3a56e482956ba86cfaf1be5da0577b562dbd
jei.cat <jarvis8617001@gamil.com>
2022-06-08 02:17:51 +0800

    用localStorage解決關閉瀏覽器資料消失的問題

--- app.js
+++ app.js
@@ -60,5 +60,73 @@ button.addEventListener("click", (e) => {
   todo.appendChild(checkButton);
   todo.appendChild(trashButton);
 
+  // 把代辦事項的資料保存在瀏覽器的Local Storage
+  let myTodo = {
+    todoText: todoText,
+    todoMonth: todoMonth,
+    todoDate: todoDate,
+  };
+
+  let myList = localStorage.getItem("list");
+  if (myList == null) {
+    localStorage.setItem("list", JSON.stringify([myTodo]));
+  } else {
+    let myListArray = JSON.parse(myList);
+    myListArray.push(myTodo);
+    localStorage.setItem("list", JSON.stringify(myListArray));
+  }
+
   section.appendChild(todo);
 });
+
+// 網頁載入時
+let myList = localStorage.getItem("list");
+if (myList !== null) {
+  let myListArray = JSON.parse(myList);
+
+  myListArray.forEach((item) => {
+    /* 把資料加進<section></section> */
+    let todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    let text = document.createElement("p");
+    text.classList.add("todo-text");
+    text.innerText = item.todoText;
+
+    let time = document.createElement("p");
+    time.classList.add("todo-time");
+    time.innerText = `${item.todoMonth} / ${item.todoDate}`;
+
+    todo.appendChild(text);
+    todo.appendChild(time);
+
+    /* 加進Font Awesome功能按鈕 */
+    let checkButton = document.createElement("button");
+    let trashButton = document.createElement("button");
+
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    checkButton.classList.add("checkButton");
+
+    trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    trashButton.classList.add("trashButton");
+
+    //設計按鈕劃刪除線功能
+    checkButton.addEventListener("click", () => {
+      // 如果沒有class:toggle就加進去 , 有就移除掉
+      todo.classList.toggle("done");
+    });
+    // 設計按鈕移除功能
+    trashButton.addEventListener("click", () => {
+      // 設計移除時由大變小動畫
+      todo.style.animation = "scaleDown 0.2s ease";
+      addEventListener("animationend", () => {
+        todo.remove();
+      });
+    });
+
+    todo.appendChild(checkButton);
+    todo.appendChild(trashButton);
+
+    section.appendChild(todo);
+  });
+}
